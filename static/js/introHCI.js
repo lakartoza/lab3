d65be7492086b462d148bb93e22bbebde3fff4b9
Valentine Bondar <vbondar@ucsd.edu>
2017-01-26 13:02:50 -0800

    added shaking effect, added animation, added background colors

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,19 +8,42 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+function Bounce (element) {
+	$(element).animate({
+		width:420
+	}, 100);
+	$(element).animate({
+		width:400
+	}, 200);
+};
+
+
+
 function initializePage() {
+
+	//Initial Shake 
+	setInterval((function(){
+		$( "#testjs" ).effect({
+		  	effect:"shake", 
+		  	direction:"up",
+		  	distance:2,
+		  	times:2
+		});
+	}),4000);
+
+
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 		$("#testjs").text("Please wait...");
 			$("#testjs").hide();
-			// $(".jumbotron").animate({
-			// 	textAlign:"center"
-			// }, 500);
 			$('.jumbotron').toggleClass("active");
 			$('.jumbotron p').toggleClass("active");
 			$('.jumbotron').append("<img src='images/good_job.jpg' alt='Lorem Pixel image' class='jumba_img'>");
 			$('.jumbotron').append("<h1>Good job</h1>");
+			Bounce(".jumbotron img");
+			
 	});
+
 
 
 	$(".project").click(function(e) {
